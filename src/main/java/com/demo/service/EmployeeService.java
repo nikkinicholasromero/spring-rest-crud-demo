83d74b88189fd0453ee6fbed4ac5a27125615b45
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2023-03-28 13:09:56 +0800

    Added Transformer layer

--- src/main/java/com/demo/service/EmployeeService.java
+++ src/main/java/com/demo/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.demo.database.entity.Employee;
 import com.demo.database.repository.EmployeeRepository;
 import com.demo.dto.EmployeeDto;
 import com.demo.exception.EmployeeNotFound;
+import com.demo.transformer.EmployeeTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +15,12 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private EmployeeTransformer employeeTransformer;
+
     public List<EmployeeDto> findAll() {
         return employeeRepository.findAll().stream()
-                .map(employee -> new EmployeeDto(
-                        employee.id(),
-                        employee.firstName(),
-                        employee.lastName(),
-                        employee.numberOfDependents(),
-                        employee.height(),
-                        employee.weight()))
+                .map(employeeTransformer::transform)
                 .toList();
     }
 
@@ -30,12 +28,6 @@ public class EmployeeService {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFound("Employee with id " + id + " not found. "));
 
-        return new EmployeeDto(
-                employee.id(),
-                employee.firstName(),
-                employee.lastName(),
-                employee.numberOfDependents(),
-                employee.height(),
-                employee.weight());
+        return employeeTransformer.transform(employee);
     }
 }
---
+++ src/main/java/com/demo/transformer/EmployeeTransformer.java
@@ -0,0 +1,19 @@
+package com.demo.transformer;
+
+import com.demo.database.entity.Employee;
+import com.demo.dto.EmployeeDto;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeTransformer {
+    public EmployeeDto transform(Employee employee) {
+        return new EmployeeDto(
+                StringUtils.trimToNull(employee.id()),
+                StringUtils.trimToNull(employee.firstName()),
+                StringUtils.trimToNull(employee.lastName()),
+                employee.numberOfDependents(),
+                employee.height(),
+                employee.weight());
+    }
+}
