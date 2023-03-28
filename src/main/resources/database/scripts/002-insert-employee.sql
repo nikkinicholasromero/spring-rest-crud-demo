646723b98f3d62fa2553a24ccd19d7279501d749
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2023-03-28 13:33:20 +0800

    Recreated SQL scripts

---
+++ src/main/resources/database/scripts/002-insert-employee.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMPLOYEE VALUES
+ ('fe38d09d-41c3-4366-90a8-4bcd392e06cd', 'Nikki Nicholas', 'Romero', 3, 177.52, 72.53, '2023-01-01', '18:00:00'),
+ ('63c1a388-c80c-4244-b1c5-fcd1eb440e45', 'Leslie Anne', 'Sayin', 3, 152.59, 64.77, '2023-02-01', '09:00:00');
