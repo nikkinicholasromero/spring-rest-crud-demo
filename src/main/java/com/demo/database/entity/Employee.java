ac3640d37162d409f45db7990fc9e6fc42d929ae
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2023-03-28 11:53:38 +0800

    Added Employee JPA Entity

---
+++ src/main/java/com/demo/database/entity/Employee.java
@@ -0,0 +1,65 @@
+package com.demo.database.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Entity
+public class Employee {
+    @Id
+    private String id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    protected Employee() {
+        // Note : Required by JPA. Do not use.
+    }
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        return new EqualsBuilder().append(id, employee.id).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(id).toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/demo/database/entity/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.demo.database.entity;
+
+import org.springframework.data.repository.Repository;
+
+public interface EmployeeRepository extends Repository<Employee, String> {
+}
