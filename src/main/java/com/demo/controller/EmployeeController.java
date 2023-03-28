10b3c0a943576cbce5a7d8903c62ba13cdcaafe4
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2023-03-28 11:26:58 +0800

    Injected EmployeeService in EmployeeController

--- src/main/java/com/demo/controller/EmployeeController.java
+++ src/main/java/com/demo/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.demo.controller;
 
 import com.demo.dto.EmployeeDto;
+import com.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping
     public List<EmployeeDto> findAll() {
-        return List.of();
+        return employeeService.findAll();
     }
 }
